916b6f2ccd68f9721d77b19f942b36c486516ba7
GPrashanthkumar <garlapally@gmail.com>
2017-12-16 13:29:26 +0530

    first checkin

---
+++ src/main/java/MyMavenGroup/SampleMavenProject/App.java
@@ -0,0 +1,13 @@
+package MyMavenGroup.SampleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
